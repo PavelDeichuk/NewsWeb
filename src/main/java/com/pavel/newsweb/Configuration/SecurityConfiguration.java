d5c8f0532414e644a84ca7f1bc500d283cf4ac9f
pavel <deychuk@inbox.ru>
2022-09-13 14:43:01 +0300

    Add UserDetailService from Security

--- src/main/java/com/pavel/newsweb/Configuration/SecurityConfiguration.java
+++ src/main/java/com/pavel/newsweb/Configuration/SecurityConfiguration.java
@@ -1,14 +1,23 @@
 package com.pavel.newsweb.Configuration;
 
+import com.pavel.newsweb.Service.impl.UsersDetailService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UsersDetailService usersDetailService;
+
+    public SecurityConfiguration(UsersDetailService usersDetailService) {
+        this.usersDetailService = usersDetailService;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
@@ -16,6 +25,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        auth.userDetailsService(usersDetailService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
