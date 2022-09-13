58a07bd11d1d625446d684d617cb6d1e3dea9fb5
pavel <deychuk@inbox.ru>
2022-09-13 14:04:40 +0300

    Change UsersFactories(UsersEntity to dto)

--- src/main/java/com/pavel/newsweb/Factories/UsersFactories.java
+++ src/main/java/com/pavel/newsweb/Factories/UsersFactories.java
@@ -1,5 +1,16 @@
 package com.pavel.newsweb.Factories;
 
-public class UsersFactories {
+import com.pavel.newsweb.Dto.UsersDto;
+import com.pavel.newsweb.Entity.UsersEntity;
 
+public class UsersFactories {
+     public static final UsersDto MAKE_DTO(UsersEntity usersEntity){
+         return UsersDto
+                 .builder()
+                 .id(usersEntity.getId())
+                 .username(usersEntity.getUsername())
+                 .email(usersEntity.getEmail())
+                 .roles(usersEntity.getRoles())
+                 .build();
+     }
 }
