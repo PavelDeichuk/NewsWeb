4d062884fce95d087f2192cfa597009752f33d4f
pavel <deychuk@inbox.ru>
2022-09-13 13:41:09 +0300

    Update SecurityConfiguration

--- src/main/java/com/pavel/newsweb/Configuration/SecurityConfiguration.java
+++ src/main/java/com/pavel/newsweb/Configuration/SecurityConfiguration.java
@@ -1,9 +1,21 @@
 package com.pavel.newsweb.Configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfiguration {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
 }
