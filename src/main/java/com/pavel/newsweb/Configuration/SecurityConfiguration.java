ba2f076b56916aeab0b528519c7112429bf90e43
Pavel <pavel@MacBook-Air-Pavel.local>
2022-09-13 13:38:52 +0300

    Initial commit

---
+++ src/main/java/com/pavel/newsweb/Configuration/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.pavel.newsweb.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+}
---
+++ src/test/java/com/pavel/newsweb/NewsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavel.newsweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
