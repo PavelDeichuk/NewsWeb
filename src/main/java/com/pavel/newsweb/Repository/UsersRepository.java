be8ebe42bae6882afa97c8160a728aaf7f64c0a9
pavel <deychuk@inbox.ru>
2022-09-13 14:06:15 +0300

    Change UsersRepository

--- src/main/java/com/pavel/newsweb/Repository/UsersRepository.java
+++ src/main/java/com/pavel/newsweb/Repository/UsersRepository.java
@@ -3,5 +3,12 @@ package com.pavel.newsweb.Repository;
 import com.pavel.newsweb.Entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<UsersEntity,Long> {
+    Optional<UsersEntity> findByUsername(String username);
+
+    Optional<UsersEntity> findByEmail(String email);
+
+    Optional<UsersEntity> findByActivationcode(String activationcode);
 }
