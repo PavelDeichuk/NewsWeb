fbc41a4ba6d07a83b352f80787256c6265d5053d
pavel <deychuk@inbox.ru>
2022-09-20 13:39:33 +0300

    Add validation, and news many to many

--- src/main/java/com/pavel/newsweb/Entity/UsersEntity.java
+++ src/main/java/com/pavel/newsweb/Entity/UsersEntity.java
@@ -3,6 +3,11 @@ package com.pavel.newsweb.Entity;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+import java.util.UUID;
 
 @Entity
 @Getter
@@ -16,16 +21,24 @@ public class UsersEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "Username null!")
+    @Size(min = 3, max = 20, message = "Username is min 3, max 20")
     private String username;
 
+    @NotNull(message = "Password null!")
     private String password;
 
     @Transient
     private String password2;
 
+    @Email(message = "email is not valid")
+    @Size(max = 24, message = "email is max size is 24")
     private String email;
 
     private String roles;
 
-    private String activationcode;
+    private String activationcode = UUID.randomUUID().toString();
+
+    @ManyToMany(mappedBy = "usersEntities")
+    private List<NewsEntity> newsEntities;
 }
