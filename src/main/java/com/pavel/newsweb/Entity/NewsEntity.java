57f9875d70bb6b33efdb6436fa97e1d50195d22a
pavel <deychuk@inbox.ru>
2022-09-20 14:30:50 +0300

    Add ToString annotation for NewsEntity, and delete getter and setter add annotation getter setter

--- src/main/java/com/pavel/newsweb/Entity/NewsEntity.java
+++ src/main/java/com/pavel/newsweb/Entity/NewsEntity.java
@@ -7,13 +7,17 @@ import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @Table(name = "news")
 public class NewsEntity {
     @Id
@@ -21,9 +25,11 @@ public class NewsEntity {
     private Long id;
 
     @NotNull(message = "Title null!")
+    @Size(min = 4, max = 256, message = "title size is min 4, max 256!")
     private String title;
 
     @NotNull(message = "Description null!")
+    @Size(min = 3, max = 1024, message = "description size is min 3, max 1024")
     private String description;
 
     @UpdateTimestamp
@@ -33,6 +39,7 @@ public class NewsEntity {
     private LocalDateTime createby;
 
     @ManyToMany(mappedBy = "newsEntity")
+    @ToString.Exclude
     @JsonIgnore
     private List<CategoryEntity> categoryEntities;
 
@@ -41,62 +48,7 @@ public class NewsEntity {
             name = "news_users",
             joinColumns = @JoinColumn(name = "news_id"),
             inverseJoinColumns = @JoinColumn(name = "users_id"))
+    @ToString.Exclude
     private List<UsersEntity> usersEntities;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getUpdateby() {
-        return updateby;
-    }
-
-    public void setUpdateby(LocalDateTime updateby) {
-        this.updateby = updateby;
-    }
-
-    public LocalDateTime getCreateby() {
-        return createby;
-    }
-
-    public void setCreateby(LocalDateTime createby) {
-        this.createby = createby;
-    }
-
-    public List<CategoryEntity> getCategoryEntities() {
-        return categoryEntities;
-    }
-
-    public void setCategoryEntities(List<CategoryEntity> categoryEntities) {
-        this.categoryEntities = categoryEntities;
-    }
-
-    public List<UsersEntity> getUsersEntities() {
-        return usersEntities;
-    }
-
-    public void setUsersEntities(List<UsersEntity> usersEntities) {
-        this.usersEntities = usersEntities;
-    }
 
 }
