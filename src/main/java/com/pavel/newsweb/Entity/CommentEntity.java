5477961285f09060665bf753d625e066c00076f0
pavel <deychuk@inbox.ru>
2022-09-20 13:47:18 +0300

    Add CommentEntity

---
+++ src/main/java/com/pavel/newsweb/Entity/CommentEntity.java
@@ -0,0 +1,33 @@
+package com.pavel.newsweb.Entity;
+
+import lombok.*;
+import net.minidev.json.annotate.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comment")
+public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min = 3, message = "Comment description is min 3")
+    @NotNull(message = "description is null!")
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private UsersEntity usersEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "news_id")
+    private NewsEntity newsEntity;
+}
