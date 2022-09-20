5295148ddc35df9e98ae3cb2cf8f80c5a8c21f08
pavel <deychuk@inbox.ru>
2022-09-20 15:13:44 +0300

    Add AccessDeniedHandler

---
+++ src/main/java/com/pavel/newsweb/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,17 @@
+package com.pavel.newsweb.handler;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.getWriter().println("Your role doesn't fit, if you have not activated your account, please activate it!");
+    }
+}
